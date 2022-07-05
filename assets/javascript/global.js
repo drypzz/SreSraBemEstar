ef36f17888b561e083a9d0115f5936c0b11c2864
elseif <fdfjonfmd@gmail.com>
2022-07-05 14:31:07 -0300

    att geral

---
+++ assets/javascript/global.js
@@ -0,0 +1,18 @@
+
+// active function
+
+document.querySelectorAll('.navbar-item a').forEach(isActive => {
+    isActive.addEventListener('click', function(){
+        document.querySelector('.navbar-menu').querySelector('.active').classList.remove('active')
+        isActive.classList.add('active')
+    })
+});
+
+// click navbar
+
+document.querySelector('#mobile-menu').addEventListener('click',
+    () => {
+        document.querySelector('#mobile-menu').classList.toggle('is-active')
+        document.querySelector('.navbar-menu').classList.toggle('active')
+    }
+);
