78d991cc5bf6e26edf5337434866ab77a5081ea4
GUSTAVO ADRIANO ALVES PALMEIRA <gustavo.palmeira@alunos.sc.senac.br>
2022-08-25 13:33:45 -0300

    att global

---
+++ assets/javascript/functions.js
@@ -0,0 +1,10 @@
+
+// function
+
+date = function(){
+    return document.querySelector('#date').innerHTML = new Date().getFullYear();
+};
+
+// callback
+
+date();
---
+++ assets/javascript/navbar.js
@@ -0,0 +1,16 @@
+
+// function
+
+document.querySelectorAll('.navbar-item a').forEach(isActive => {
+    isActive.addEventListener('click', function(){
+        document.querySelector('.navbar-menu').querySelector('.active').classList.remove('active')
+        isActive.classList.add('active')
+    })
+});
+
+document.querySelector('#mobile-menu').addEventListener('click',
+    () => {
+        document.querySelector('#mobile-menu').classList.toggle('is-active')
+        document.querySelector('.navbar-menu').classList.toggle('active')
+    }
+);
