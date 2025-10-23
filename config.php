8a48bcb865320e2bef6c1e97900f0addd890a7dd
Lincoln <Lincolnnovais15a@gmail.com>
2025-10-23 20:44:21 -0300

    feat: update database configuration to use environment variables

--- config.php
+++ config.php
@@ -6,11 +6,11 @@
 <?php
     $config = array(
         $database = array(
-            'host' => 'localhost',
-            'port' => '3306',
-            'dbname' => 'sbe',
-            'user' => 'root',
-            'password' => '',
+            'host' => getenv('DB_HOST') ?: 'localhost',
+            'port' => getenv('DB_PORT') ?: '3306',
+            'dbname' => getenv('DB_NAME') ?: 'sbe',
+            'user' => getenv('DB_USER') ?: 'root',
+            'password' => getenv('DB_PASS') ?: null,
         ),
     );
 ?>
